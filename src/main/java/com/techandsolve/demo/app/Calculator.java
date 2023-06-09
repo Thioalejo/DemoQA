67274822e3d423845564f229982d4a4890a958b4
Johnny <alejo.saraza@gmail.com>
2023-06-09 01:38:58 -0500

    Se crea proyecto base

---
+++ src/main/java/com/techandsolve/demo/app/Calculator.java
@@ -0,0 +1,7 @@
+package com.techandsolve.demo.app;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/techandsolve/demo/acceptancetests/AcceptanceTestSuite.java
@@ -0,0 +1,9 @@
+package com.techandsolve.demo.acceptancetests;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features="src/test/resources/features/", glue="com.techandsolve.demo")
+public class AcceptanceTestSuite {}
