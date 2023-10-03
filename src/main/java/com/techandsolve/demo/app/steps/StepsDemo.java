a77225f4e0e1bb7e95bab52bee80084010ac9a44
Johnny <alejo.saraza@gmail.com>
2023-10-03 14:26:32 -0500

    Se deja base para iniciar desde cero

--- src/main/java/com/techandsolve/demo/app/steps/StepsDemo.java
+++ src/main/java/com/techandsolve/demo/app/steps/StepsDemo.java
@@ -2,7 +2,6 @@ package com.techandsolve.demo.app.steps;
 
 
 import com.techandsolve.demo.app.iu.PageHome;
-import com.techandsolve.demo.app.iu.PaginaLoginDemoQA;
 import net.thucydides.core.annotations.Step;
 import net.thucydides.core.pages.PageObject;
 
@@ -10,21 +9,4 @@ import static org.junit.Assert.assertEquals;
 
 public class StepsDemo extends PageObject {
 
-    PageHome pageHome;
-    PaginaLoginDemoQA paginaLoginDemoQA;
-    public void OpenPage() {
-        pageHome.open();
-    }
-
-    @Step
-    public void diligenciarFormularioLogin(String userName, String password) {
-        paginaLoginDemoQA.ingresarUserName(userName);
-        paginaLoginDemoQA.ingresarPassword(password);
-        paginaLoginDemoQA.darClicBotonLogin();
-    }
-
-    @Step
-    public void validarTituloDespuesDeLogin(String tituloLogin) {
-        assertEquals(tituloLogin, paginaLoginDemoQA.buscarTituloDespuesDeLogin());
-    }
 }
--- src/test/java/com/techandsolve/demo/steps/StepDefinitions.java
+++ src/test/java/com/techandsolve/demo/steps/StepDefinitions.java
@@ -12,16 +12,5 @@ public class StepDefinitions {
     @Steps
     StepsDemo stepsDemo;
 
-    @Dado("que yo me encuentre la pagina de inicio de sesion")
-    public void que_yo_me_encuentre_la_pagina_de_inicio_de_sesion() {
-        stepsDemo.OpenPage();
-    }
-    @Cuando("cuando logre iniciar sesion con el usuario {string} y clave {string}")
-    public void cuando_logre_iniciar_sesion_con_el_usuario_y_clave(String userName, String password) {
-        stepsDemo.diligenciarFormularioLogin(userName, password);
-    }
-    @Entonces("deberia ver mi usuario {string}")
-    public void deberia_ver_mi_usuario(String tituloLogin) {
-        stepsDemo.validarTituloDespuesDeLogin(tituloLogin);
-    }
+
 }
